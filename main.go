6c4aaf3cd50a8c3e78b7afc233ca1a03d3197f6f
srsuper <aot5310@gmail.com>
2020-05-08 00:54:21 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,63 +1,97 @@
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"strconv"
+    "fmt"
+    "log"
+    "net/http"
+    "net/url"
+    "os"
 
-	"github.com/line/line-bot-sdk-go/linebot"
+    "github.com/dongri/line-bot-sdk-go/linebot"
 )
 
-var bot *linebot.Client
+var botClient *linebot.Client
 
 func main() {
-	var err error
-	bot, err = linebot.New(os.Getenv("ChannelSecret"), os.Getenv("ChannelAccessToken"))
-	log.Println("Bot:", bot, " err:", err)
-	http.HandleFunc("/callback", callbackHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
+    channelID := os.Getenv("LINE_CHANNEL_ID")
+    channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+    mid := os.Getenv("LINE_MID")
+    proxyURL := getProxyURL() // can set nil if not need
+
+    botClient = linebot.NewClient(channelID, channelSecret, mid, proxyURL)
+
+    // EventHandler
+    var myEvent linebot.EventHandler = NewEventHandler()
+    botClient.SetEventHandler(myEvent)
+
+    http.HandleFunc("/callback", callbackHandler)
+    port := os.Getenv("PORT")
+    addr := fmt.Sprintf(":%s", port)
+    http.ListenAndServe(addr, nil)
 }
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	events, err := bot.ParseRequest(r)
+    log.Print("=== callback ===")
+}
 
-	if err != nil {
-		if err == linebot.ErrInvalidSignature {
-			w.WriteHeader(400)
-		} else {
-			w.WriteHeader(500)
-		}
-		return
-	}
+func getProxyURL() *url.URL {
+    proxyURL, err := url.Parse(os.Getenv("PROXY_URL"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    return proxyURL
+}
 
-	for _, event := range events {
-		if event.Type == linebot.EventTypeMessage {
-			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				quota, err := bot.GetMessageQuota().Do()
-				if err != nil {
-					log.Println("Quota err:", err)
-				}
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK! remain message:"+strconv.FormatInt(quota.Value, 10))).Do(); err != nil {
-					log.Print(err)
-				}
-			}
-		}
-	}
+// BotEventHandler ...
+type BotEventHandler struct {
+}
+
+// NewEventHandler ...
+func NewEventHandler() *BotEventHandler {
+    return &BotEventHandler{}
+}
+
+// OnAddedAsFriendOperation ...
+func (be *BotEventHandler) OnAddedAsFriendOperation(mids []string) {
+    botClient.SendText(mids, "友達追加してくれてありがとうね！")
+}
+
+// OnBlockedAccountOperation ...
+func (be *BotEventHandler) OnBlockedAccountOperation(mids []string) {
+    botClient.SendText(mids, "あらら,,, (このメッセージは届かない)")
+}
+
+// OnTextMessage ...
+func (be *BotEventHandler) OnTextMessage(from, text string) {
+    log.Print("=== Received Text ===")
+}
+
+// OnImageMessage ...
+func (be *BotEventHandler) OnImageMessage(from string) {
+    log.Print("=== Received Image ===")
+}
+
+// OnVideoMessage ...
+func (be *BotEventHandler) OnVideoMessage(from string) {
+    log.Print("=== Received Video ===")
+}
+
+// OnAudioMessage ...
+func (be *BotEventHandler) OnAudioMessage(from string) {
+    log.Print("=== Received Audio ===")
+}
+
+// OnLocationMessage ...
+func (be *BotEventHandler) OnLocationMessage(from, title, address string, latitude, longitude float64) {
+    log.Print("=== Received Location ===")
+}
+
+// OnStickerMessage ...
+func (be *BotEventHandler) OnStickerMessage(from, stickerPackageID, stickerID, stickerVersion, stickerText string) {
+    log.Print("=== Received Sticker ===")
+}
+
+// OnContactMessage ...
+func (be *BotEventHandler) OnContactMessage(from, MID, displayName string) {
+    log.Print("=== Received Contact ===")
 }
