daeff8392a91ca6468f926d03dd80b18ade2eb3f
srsuper <aot5310@gmail.com>
2020-05-13 00:09:40 +0700

    อัพเดท
    ฆฆ

--- main.go
+++ main.go
@@ -19,6 +19,9 @@ import (
 	"os"
 	"html/template"
 	"strings"
+	"vendor/linebot"
+	"vendor/golang.org"
+	"vendor/examples"
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
@@ -29,24 +32,24 @@ func main(){
 	bot, err = linebot.New(os.Getenv("3eda0f01f6b2fe05aace973e39707edc"), os.Getenv("XuAPgE5eH13Hbgj7mSSCmqe5wheTgVDhiE805ypPKx1hyHXCXLgshl02rpLCe+rUUVTfBE6SkoXrkRD0c1omm6o8RFZMgCETtwF7nDTKSg3PDQG6OIHE2npC1e3YfWXhvBMcXBwFrF5zE8s9T83cgQdB04t89/1O/w1cDnyilFU="))
 	log.Println("Bot:", bot, " err:", err)
 	//Default: Hello World!
-	http.HandleFunc("/", HelloServer)              		
+	http.HandleFunc("/", HelloServer)
 	//User input form
-	http.HandleFunc("/InputMsg", InputMsgHandler)  		
+	http.HandleFunc("/InputMsg", InputMsgHandler)
 	//Send message by user input
 	http.HandleFunc("/SendMsg", SendMsgHandler)
-	//1. Can Reply message when use Line 
+	//1. Can Reply message when use Line
 	//2. Use html Get to Send message
-	//EXAMPLE: https://loaclhost/SendMsg?type=SendMsg&uid=[UserID]&msg=[Message you want to send] 
-	http.HandleFunc("/SendLineMsg", SendLineMsgHandler)  
-	
+	//EXAMPLE: https://loaclhost/SendMsg?type=SendMsg&uid=[UserID]&msg=[Message you want to send]
+	http.HandleFunc("/SendLineMsg", SendLineMsgHandler)
+
 	port := os.Getenv("PORT")
 	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)	
+	http.ListenAndServe(addr, nil)
 }
 func SendLineMsgHandler(w http.ResponseWriter, r *http.Request) {
 	//Parse HttpRequest
 	events, err := bot.ParseRequest(r)
-	log.Println("Events:", events, " err:", err)  
+	log.Println("Events:", events, " err:", err)
 	if err != nil {
 		if err == linebot.ErrInvalidSignature {
 			w.WriteHeader(400)
@@ -73,32 +76,32 @@ func SendLineMsgHandler(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 		}
-	}	
+	}
 }
 func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
 	//Parse HttpRequest
 	//notice: if not use ParseForm, you can't get the values
-	r.ParseForm()   
+	r.ParseForm()
 	val := [3][2]string{{"type", ""}, {"uid", ""}, {"msg", ""}}
-	
+
 	//Put the values to array
 	i := 0
-	for k, v := range r.Form {			
-		fmt.Println("key:", k)		
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 		if k == "type" {
-			val[0][1] = strings.Join(v, "")			
+			val[0][1] = strings.Join(v, "")
 		}
 		if k == "uid" {
-			val[1][1] = strings.Join(v, "")			
+			val[1][1] = strings.Join(v, "")
 		}
 		if k == "msg" {
-			val[2][1] = strings.Join(v, "")			
+			val[2][1] = strings.Join(v, "")
 		}
 		i++
 	}
 	//Print values on the output Page
-	for i := 0; i < 3; i++ {		
+	for i := 0; i < 3; i++ {
 		fmt.Fprint(w, val[i][0])
 		fmt.Fprint(w, " :\n")
 		fmt.Fprintln(w, val[i][1])
@@ -124,14 +127,14 @@ func InputMsgHandler(w http.ResponseWriter, r *http.Request) {
         fmt.Println("User ID:", r.Form["method"])
         fmt.Println("User ID:", r.Form["uid"])
         fmt.Println("Message:", r.Form["msg"])
-    }		
+    }
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	//Parse the parameter from httpReuest
 	//notice: if not use ParseForm, you can't get the values
-	r.ParseForm()       
+	r.ParseForm()
 	//Print on the Server
-    fmt.Println(r.Form) 
+    fmt.Println(r.Form)
     fmt.Println("path", r.URL.Path)
     fmt.Println("scheme", r.URL.Scheme)
     fmt.Println(r.Form["url_long"])
